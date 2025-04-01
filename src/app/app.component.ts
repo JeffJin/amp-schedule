deb3c1c3ba2527923f7469d7009d6385b89b4c39
Jeff Jin <jeff@jeffjin.com>
2025-03-31 23:00:36 -0400

    page refresh authenticator issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
-import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
-import { Amplify } from 'aws-amplify';
 import { Hub } from 'aws-amplify/utils';
 import { CONNECTION_STATE_CHANGE, ConnectionState } from 'aws-amplify/data';
-
-import outputs from '../../amplify_outputs.json';
+import { getCurrentUser } from 'aws-amplify/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ RouterOutlet, AmplifyAuthenticatorModule, JsonPipe ]
+  imports: [ RouterOutlet, AmplifyAuthenticatorModule ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Adworks Scheduling';
 
   constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure(outputs);
   }
 
-  ngOnInit(): void {
+
+  async ngOnInit(): Promise<void> {
+    await this.currentAuthenticatedUser();
     Hub.listen('api', (data: any) => {
       const { payload } = data;
       if (payload.event === CONNECTION_STATE_CHANGE) {
@@ -29,5 +27,17 @@ export class AppComponent {
         console.log('connectionState', connectionState);
       }
     });
+
   }
+
+  async currentAuthenticatedUser() {
+    try {
+      const { username, userId, signInDetails } = await getCurrentUser();
+      console.log('currentAuthenticatedUser', username, userId, signInDetails);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideAppInitializer } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import outputs from '../../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+
+function initializeApp() {
+  return () => {
+    Amplify.configure(outputs);
+    return Promise.resolve();
+  };
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAppInitializer(initializeApp())
+  ],
 };
